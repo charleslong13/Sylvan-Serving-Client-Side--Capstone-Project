417f468387eec73e45ffe2ba7facabab93988574
charleslong13 <charleslong13@icloud.com>
2022-03-09 16:47:45 -0600

    csl-firstCommit

---
+++ src/components/home/home.js
@@ -0,0 +1,46 @@
+export const AboutUs = () => {
+    return (
+    <>
+        <div className="aboutUs">
+        <h3 className="aboutUsTitle">
+                    <i>About Us</i>
+                </h3>
+                <div className="aboutUsBody">
+                    <h3 className="aboutUsTitle">
+                        <div>Paranormal Investigators based in Middle Tennessee</div>
+                    </h3>
+    
+                        <i>Elite Vixen Paranormal</i> (EVP) is an all-female paranormal investigation team based out of middle Tennessee with over 40 years of paranormal research experience. 
+                        The team was founded in 2018 by Samantha Glumm and Chinelle Gwartney, and includes Kayla Cormier, Jessica Ward, Rayna Michelle, and Janice Landis. 
+                        We are a tight-knit group of badass women who are scientifically driven, and are providing female representation in a male-driven industry.
+                        We are here to represent the women in the field, and do things just as our male colleagues. 
+                        We use scientific based investigative techniques, and empirical practices in our investigations and research.
+                        <div><div></div></div>
+                <h4>What We Do:</h4> 
+                        Paranormal investigation group, historical researchers, video/audio editing, filming, available for podcasts, public events, charity events, private investigations, and much more!
+                        <div><div></div></div>
+                <i>Please feel free to contact us!</i>
+                        </div>
+                <div>
+                        <h4><u>Business Hours</u></h4>
+                            Monday - Friday: Open 24 Hours 
+
+                </div>
+              
+                <div className="evpImages">
+                    <div className="img">
+                        <img src="https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/132443884_769581996962733_43528084583133406_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=s9jzmz0Knq0AX_HP0dw&tn=vX3xHMaWoKXFV2xO&_nc_ht=scontent-atl3-2.xx&oh=00_AT9uGQzJDc4DTPfuOkfqVMtNA8iOXAImkzN9ANiSP9P6jQ&oe=61DD93B9" alt="1" />
+                    </div>
+                <div className="img">
+                        <img src="https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/130235536_758141754773424_8563613231586682612_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=v5KQE-irkUsAX_-tSho&_nc_ht=scontent-atl3-2.xx&oh=00_AT8ripLCyDBPVAVxbDmjE7yHmNlD4ms6TAg9MbCELMsGSA&oe=61DD2E2D" alt="2" />
+                </div>
+           
+                <div className="img">
+                        <img src="https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/127663854_749571258963807_4610376015619719451_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=3jVaspf2CGoAX-BSgpH&_nc_ht=scontent-atl3-2.xx&oh=00_AT99pAbtjhgSqDEV10K-5Py9UDbt430ZnSc6cHwAxWgnIA&oe=61E05C97" alt="5" />
+                </div>
+                <div className="img">
+                        <img src="https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/123508335_731670867420513_1737007096390177042_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=HfIMgspnIKcAX_DNGco&_nc_ht=scontent-atl3-2.xx&oh=00_AT8iXj6p5arZAjImg1qP--52KBU861g48uCbBxHP8F0-1Q&oe=61DF391B" alt="6" />
+                </div>      
+        </div></div>
+    </>
+    )}
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link, useHistory } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const history = useHistory()
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                <Link className="navbar_item" to="/">Home</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar_item" to="/deckfeed">Deck List Feed</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar_item" to="/events">Events</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar_item" to="/mydecks">My Decks</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar_item" to="/info">Rules And Info</Link>
+            </li>
+            
+            {
+                (localStorage.getItem("ss_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("ss_token")
+                                history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
